dc94bf6817b7ef62f853d827c0706b5cf5a97e26
huaxiabuluo <huaxiabuluo@hotmail.com>
2016-08-11 23:43:45 +0800

    动态创建模态框, 解除Ok和Cancel的click事件委托

--- bootstrap-modal-confirm.js
+++ bootstrap-modal-confirm.js
@@ -1,5 +1,5 @@
 /*!
- * Bootstrap Modal Confirm v0.0.1
+ * Bootstrap Modal Confirm v0.0.2
  * https://github.com/huaxiabuluo/bootstrap-modal-confirm
  *
  * By: Xiaolong Lu <huaxiabuluo@hotmail.com>
@@ -10,52 +10,54 @@
  * 
  */
 (function($) {
-	$.Modal = function(modalId, options) {
-		var reg = /{([^{}]*?)}/igm;
-		var $modalConfirm = $('#' + modalId);
+	var $modalDOM = $('<div id="modalId" class="modal hide fade" tabindex="-1" role="dialog" data-backdrop="static" aria-hidden="true"></div>');
+	var modalInnerHtml = 
+		'<div class="modal-header">' + 
+		    '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
+		    '<h3>{title}</h3>' +
+		'</div>' +
+		'<div class="modal-body">' +
+		    '<p>{content}</p>' +
+		'</div>' +
+		'<div class="modal-footer">' +
+		    '<a href="#" class="btn cancel" data-dismiss="modal">{btnCancel}</a>' +
+		    '<a href="#" class="btn btn-primary ok" data-dismiss="modal">{btnOk}</a>' +
+		'</div>';
+	var reg = /{([^{}]*?)}/img;
 
-		if ($.Modal.modalHtmlTemplate) {
-			var modalHtml = $.Modal.modalHtmlTemplate;
-		} else {
-			var modalHtml = $.Modal.modalHtmlTemplate = $modalConfirm.html();
-		}
+	$('body').append($modalDOM);
 
-		var _dialog = function(options) {
-			var ops = {
-				title: "title",
-				content: "content",
-				btnCancel: "Cancel",
-				btnOk: "Ok"
-			};
-
-			$.extend(ops, options);
-
-			// matchedStr: {title}, key: title
-			var html = modalHtml.replace(reg, function(matchedStr, key){
-				return ops[key];
-			});
-
-			$modalConfirm.html(html);
+	var _dialog = function(options) {
+		var ops = {
+			title: "title",
+			content: "content",
+			btnCancel: "Cancel",
+			btnOk: "Ok"
 		};
 
-		$modalConfirm.html(modalHtml);
+		$.extend(ops, options);
 
+		$modalDOM.off('click', '.ok');
+		$modalDOM.off('click', '.cancel');
+
+		// matchedStr: {title}, key: title
+		$modalDOM.html(modalInnerHtml.replace(reg, function(matchedStr, key){
+			return ops[key];
+		}));
+	};
+
+	$.Modal = function(options) {
 		_dialog(options);
-		$modalConfirm.modal('show');
-
+		$modalDOM.modal('show');
 		return {
 			do: function(OKHandler, CancelHandler) {
 				if($.isFunction(OKHandler)) {
 					// do OKHandler when click 'Ok'
-					$modalConfirm.find('.ok').click(function() {
-						OKHandler();
-					});
+					$modalDOM.on('click', '.ok', OKHandler);
 				}
 				if($.isFunction(CancelHandler)) {
 					// do CancelHandler when click 'Cancel'
-					$modalConfirm.find('.cancel').click(function() {
-						CancelHandler();
-					});
+					$modalDOM.on('click', '.cancel', CancelHandler);
 				}
 			}
 		};
